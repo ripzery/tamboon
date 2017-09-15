71bc451bf6ca6b0b689cfe59bb8244f8e5340059
Phuchit Sirimongkolsathien <phuchit.sir@ascendcorp.com>
2017-09-15 20:51:06 +0700

    Handle dismiss button clicked in success page to start first page

--- app/src/main/java/com/ripzery/tamboon/SuccessActivity.kt
+++ app/src/main/java/com/ripzery/tamboon/SuccessActivity.kt
@@ -1,12 +1,19 @@
 package com.ripzery.tamboon
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_success.*
 
 class SuccessActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_success)
+        btnDismiss.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            })
+        }
     }
 }
